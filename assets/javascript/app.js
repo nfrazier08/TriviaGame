62fc0217044f9c525275fde9a76d50e624f5777f
neliza <nicolasaelf64@gmail.com>
2017-08-23 21:12:58 -0600

    Im almost theregit add -A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,179 +1,201 @@
-//Get the page started
-$(document).ready(function(){
-
-//will hold the current question
-// var currentQuestion = 0;
-// //will hold the scores for the game
-// 	//will be displayed at the end
-// var answersCorrect = 0;
-// var answersWrong = 0; 
-// var unanswered = 0;
+//my global variables
+var counter = 5;
 var myQuestions = [{
 	question: "What are complementary colors?",
-	choice1: "Two primary colors mixed together",
-	choice2: "Two colors next to each other on the color wheel",
-	choice3: "Two colors opposite each other on the color wheel",
-	choice4: "Two colors mixed with white",
-	answer: "3"
+	choices: [	"Two primary colors mixed together",
+				"Two colors next to each other on the color wheel",
+				"Two colors opposite each other on the color wheel",
+				"Two colors mixed with white"],
+	answer: "Two colors opposite each other on the color wheel"
 },{
 	question:"What are analogous colors?",
-	choice1: "Two colors mixed with black",
-	choice2: "Two colors opposite each other on the color wheel",
-	choice3: "Three colors next to each other on the color wheel",
-	choice4: "Two complementary colors mixed togeher",
-	answer: "3"
+	choices: [	"Two colors mixed with black",
+				"Two colors opposite each other on the color wheel",
+				"Three colors next to each other on the color wheel",
+				"Two complementary colors mixed together"],
+	answer: "Three colors next to each other on the color wheel"
 },{
 	question: "What color can you add to another color to make it warmer?",
-	choice1: "White",
-	choice2: "Orange",
-	choice3: "Yellow",
-	choice4: "Red",
-	answer: "4"
+	choices: [ 	"White",
+	 			"Orange",
+				"Yellow",
+				"Red"],
+	answer: "Red"
 },{
 	question: "What color can you add to another color to make it cooler?",
-	choice1: "Black",
-	choice2: "Blue",
-	choice3: "Red",
-	choice4: "Green",
-	answer: "2"
+	choices: [ 	"Black",
+				"Blue",
+			 	"Red",
+ 				"Green"],
+	answer: "Blue"
 },{
 	question: "Who invented the circular color wheel?",
-	choice1: "Issac Newton",
-	choice2: "Wolfgang Goethe",
-	choice3: "Paul Cezanne",
-	choice4: "Gustav Klimt",
-	answer: "1"
+	choices: [ 	"Issac Newton",
+				"Wolfgang Goethe",
+				"Paul Cezanne",
+				"Gustav Klimt"],
+	answer: "Issac Newton"
 },{
 	question: "Which do you add to tint a color?",
-	choice1: "White",
-	choice2: "Black",
-	choice3: "Blue",
-	choice4: "Gray",
-	answer: "2"
+	choices: [ 	"White",
+				"Black",
+				"Blue",
+				"Gray"],
+	answer: "Black"
 },{
 	question: "Which is NOT a primary color?",
-	choice1: "Yellow",
-	choice2: "Red",
-	choice3: "Blue",
-	choice4: "Orange",
-	answer: "4"
+	choices: [	"Yellow",
+				"Red",
+				"Blue",
+	 			"Orange"],
+	answer: "Orange"
 },{
 	question: "Which is NOT an example of a complementary color pair?",
-	choice1: "Red and Red-Violet",
-	choice2: "Red-Violet and Yellow-Green",
-	choice3: "Yellow and Blue",
-	choice4: "Red and Green",
-	answer: "1"
+	choices: [	"Red and Red-Violet",
+				"Red-Violet and Yellow-Green",
+				"Yellow and Blue",
+				"Red and Green"],
+	answer: "Red and Red-Violet"
 },{
 	question: "What are secondary colors?",
-	choice1: "Two colors opposite each other on the color wheel?",
-	choice2: "Two primary colors mixed together",
-	choice3: "A tertiary color and a secondary color mixed together",
-	choice4: "Two analogous colors mixed together",
-	answer: "2"
+	choices: [	"Two colors opposite each other on the color wheel?",
+				"Two primary colors mixed together",
+				"A tertiary color and a secondary color mixed together",
+				"Two analogous colors mixed together"],
+	answer: "Two primary colors mixed together"
 },{
 	question: "Which is NOT a secondary color?",
-	choice1: "Violet",
-	choice2: "Orange",
-	choice3: "Blue",
-	choice4: "Green",
-	answer: "3"
+	choices: [	"Violet",
+				"Orange",
+				"Blue",
+				"Green"],
+	answer: "Blue"
 },{
 	question: "What is the value of a color?",
-	choice1: "The lightness or darkness of a color",
-	choice2: "The saturation of a color",
-	choice3: "How well a color pairs with its complement",
-	choice4: "Adding black to a color",
-	answer: "1"
+	choices: [	"The lightness or darkness of a color",
+				"The saturation of a color",
+				"How well a color pairs with its complement",
+				"Adding black to a color"],
+	answer: "The lightness or darkness of a color"
 },{
 	question: "Which IS tertiary color?",
-	choice1: "Orange",
-	choice2: "Green",
-	choice3: "Yellow",
-	choice4: "Blue-Violet",
-	answer: "4"
+	choices: [	"Orange",
+				"Green",
+				"Yellow",
+				"Blue-Violet"],
+	answer: "Blue-Violet"
 },{
 	question: "What are tertiary colors?",
-	choice1: "Two analogous colors",
-	choice2: "Two primary colors mixed together",
-	choice3: "A color created from a primary and a secondary color",
-	choice4: "A color with yellow added to it",
-	answer: "3"
+	choices: [	"Two analogous colors",
+				"Two primary colors mixed together",
+				"A color created from a primary and a secondary color",
+				"A color with yellow added to it"],
+	answer: "Blue-Violet"
 },{
 	question: "What is a hue?",
-	choice1: "A color with blue added to it",
-	choice2: "The purest, brightest form of a color",
-	choice3: "Two tertiary colors mixed together", 
-	choice4: "Different intensities of a color",
-	answer: "2"
+	choices: [	"A color with blue added to it",
+				"The purest, brightest form of a color",
+				"Two tertiary colors mixed together", 
+				"Different intensities of a color"],
+	answer: "The purest, brightest form of a color"
 },{
 	question: "What is a monochromatic color scheme?",
-	choice1: "Four colors evenly spaced around the color wheel",
-	choice2: "A color scheme with two or more colors",
-	choice3: "A color scheme with no color",
-	choice4: "A color scheme with one color with shades, tints and tones",
-	answer: "4"
+	choices: [	"Four colors evenly spaced around the color wheel",
+				"A color scheme with two or more colors",
+				"A color scheme with no color",
+				"A color scheme with one color with shades, tints and tones"],
+	answer: "A color scheme with one color with shades, tints and tones"
 },{
 	question: "How can you change the intensity of a color?",
-	choice1: "Add a tertiary color",
-	choice2: "Add a primary color",
-	choice3: "Add black to a color",
-	choice4: "Add the complement of the color",
-	answer: "4"
+	choices: [	"Add a tertiary color",
+				"Add a primary color",
+				"Add black to a color",
+				"Add the complement of the color"],
+	answer: "Add the complement of the color"
 },{
 	question: "Who authored Theory of Colors in 1810?",
-	choice1: "Wolfgang Goethe",
-	choice2: "John Murray",
-	choice3: "Rudolf Steiner",
-	choice4: "Louis Bertrand",
-	answer: "1"
+	choices: [ 	"Wolfgang Goethe",
+				"John Murray",
+				"Rudolf Steiner",
+				"Louis Bertrand"],
+	answer: "Wolfgang Goethe"
 },{
 	question: "Mixing of colored physical substances correspondes to:",
-	choice1: "Additive Mixing",
-	choice2: "Chroma Mixing",
-	choice3: "Subtractive Mixing",
-	choice4: "Color Mixing",
+	choices: [	"Additive Mixing",
+				"Chroma Mixing",
+				"Subtractive Mixing",
+				"Color Mixing"],
 	answer: "Subtractive Mixing"
 }];//End of myquestion object
 
-var questionDisplay = $("#question");
-var	playerChoice1 = $("#opt1");
-var	playerChoice2 = $("#opt2");
-var	playerChoice3 = $("#opt3");
-var	playerChoice4 = $("#opt4");
+var answeredRight = 0;
+var answeredWrong = 0;
 
 
-//Hide the HideShow question container of the questions
-$(".hideShow").hide();
-
-//Write function for when the start button is clicke the 
-	//container showing questions will show and start button will hide
-$("#startButton").on('click',function(){
-	$(".hideShow").show();
-	$("#startButton").hide();
+$(document).ready(function(){
 
 
-})
-
-//Timer
-var counter = 30;
+//STEP 1:Create a simple function for the timer
+	//Clear timer once it gets to 0
 var timer = $("#myTimer");
-timer.html(30)
+	timer.html("5");
+	var interval = setInterval(countdownTimer, 1000);
 
-function countDown(){
-	counter--;
-	timer.html(counter);
-	if(counter===0){
-		clearInterval(interval);
+	function countdownTimer(){
+		counter--;
+		timer.html(counter);
+		if (counter === 0){
+			clearInterval(interval);
+		}
 	}
 
-}
-var interval = setInterval(countDown, 1000);
+//STEP 2: Get questions and answers to display on the screen
+	//Clear question div 
+	$("#theQuestion").empty();
+		for (var i = 0; i < myQuestions.length; i++) {
+  			$("#theQuestion").append("<div><h4>" + myQuestions[i].question + "</div></h4>");
+  			var newUl = $("<ul>") //This is just a variable
+
+  			//second for loop is to get choices to appear with the questions in an unordered list
+  			for (var j = 0; j < myQuestions[i].choices.length; j++) {
+  				var newLi = $("<li>");
+  				newLi.text(myQuestions[i].choices[j]); //Tell computer what text to put in the new LI
+  				newUl.append(newLi);
+
+  				$("#theQuestion").append(newUl);
+  			}
+  		}//End of first loop
+
+//STEP 3: Add a click event listener to the li
+ 	//To create an event listener to a dynamically created
+ 	//element, attach the element that needs the listener 
+ 		//(in this case the "li")
+ 	//To an element that already exists
+ 		//(in this case the document)
+ 			$(document).on("click", "li", function() {
+ 				console.log($(this).text());
+ 				$(this).addClass("playerChoice");
+   			}); //End of the click function
+
+//STEP 4: Check to see if the player selection is correct answer
+	// for (var l = 0; l < myQuestions[i].answers.length; i++) {
+		if(myQuestions.answers.length === (".playerChoice")) {
+			answeredRight++
+		} else {
+			answeredWrong++
+		}
+	// }//End of for loop
+		
+	
 
 
 
 
-})//THE END--DON'T TOUCH
+
+
+}) //document.ready
+
+
 
 
 
